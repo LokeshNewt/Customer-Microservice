0254029be2d6094e41635873c80417313f1bf17d
dhanamjeyulup <dhanamjeyulup@newtglobal.com>
2016-09-23 16:43:40 +0530

    customized swagger ui for Customer Microservice

--- src/main/java/com/newt/CustomerServiceApplication.java
+++ src/main/java/com/newt/CustomerServiceApplication.java
@@ -3,18 +3,44 @@ package com.newt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
- 
-@EnableDiscoveryClient
- 
+@SpringBootApplication 
+@EnableDiscoveryClient 
 @EnableSwagger2
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
+        @Bean
+public Docket api() {                
+    return new Docket(DocumentationType.SWAGGER_2)          
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("com.newt.controller"))
+      .build()
+      .apiInfo(apiInfo());
 }
+
+private ApiInfo apiInfo() {
+    ApiInfo apiInfo = new ApiInfo(
+      "Customer Microservice",
+      "Provides services to add customer and query customer data",
+      "V.1.0",
+      "Terms of service",
+      "devopsinabox@newtglobal.com",
+      "",
+      "");
+    return apiInfo;
+}
+
+}
+
+
--- src/main/java/com/newt/controller/CustomerController.java
+++ src/main/java/com/newt/controller/CustomerController.java
@@ -1,13 +1,6 @@
 package com.newt.controller;
 
-import java.util.Date;
 import java.util.List;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,152 +9,83 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
- 
 import com.newt.model.Customer;
 import com.newt.repository.CustomerRepository;
 import com.wordnik.swagger.annotations.ApiOperation;
+import javax.ws.rs.PathParam;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-	@Autowired
-  private  CustomerRepository customerRepository;
-	
-	
-	/*@POST
-	@RequestMapping("add/{firstName}/{lastName}/{age}/{gender}/{dob}/{address1}/{address2}/{city}/{state}/{country}/{postalCode}/{phone}/{status}/{emailId}/{username}/{password}") 
-	@ApiOperation(value = "post a customer")
-	public Integer registerCustomer(@PathVariable("firstName") String firstName,@PathVariable("lastName") String lastName,@PathVariable("gender") String gender,@PathVariable("age") Integer age,@PathVariable("dob") Date dob,@PathVariable("address1") String address1,@PathVariable("address2") String address2,@PathVariable("city") String city,@PathVariable("state") String state,@PathVariable("country") String country,@PathVariable("postalCode") String postalCode,@PathVariable("phone") String phone,@PathVariable("status") String status,@PathVariable("emailId") String emailId,@PathVariable("username") String username,@PathVariable("password") String password)
-	{
-		//System.out.println("Adding Employee Details===============:"+name+" "+" "+dept+" "+sal+" "+yoexp);
-		System.out.println("Adding customer details into DB....");
-		 Customer customer = new Customer();
-		 customer.setFirstName(firstName);
-		 customer.setLasstName(lastName);
-		 customer.setAge(age);
-		 customer.setDob(dob);
-		 customer.setAddress1(address1);
-		 customer.setAddress2(address2);
-		 customer.setCity(city);
-		 customer.setState(state);
-		 customer.setCountry(country);
-		 customer.setPostalCode(postalCode);
-		 customer.setPhone(phone);
-		 customer.setStatus(status);
-		 customer.setEmailId(emailId);
-		 customer.setUsername(username);
-		 customer.setPassword(password);
-		 customerRepository.save(customer);
-		 System.out.println("Successfully added...!");
-	return customer.getCustomerId();
-		
-	}*/
-	//creating customer services
-	
-	@ApiOperation(value = "post a customer")
-    @RequestMapping(method = RequestMethod.POST)
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @ApiOperation(value = "post a customer")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public Customer registerCustomer(@RequestBody Customer customer) {
-		Customer customer1 =  (Customer) customerRepository.findAll();
-		if(customer1.getUsername().equalsIgnoreCase(customer.getUsername())||customer1.getEmailId().equalsIgnoreCase(customer.getEmailId())){
-			System.out.println("Uesr anme and email id is exit plz try another one");
-		}
-		else{
-			customerRepository.save(customer);
-		}
-		
-		
-        return  customer;
+        Customer customer1 = (Customer) customerRepository.findAll();
+        if (customer1.getUsername().equalsIgnoreCase(customer.getUsername()) || customer1.getEmailId().equalsIgnoreCase(customer.getEmailId())) {
+            System.out.println("Uesr anme and email id is exit plz try another one");
+        } else {
+            customerRepository.save(customer);
+        }
+
+        return customer;
     }
-	
-	
-	/*@RequestMapping(value="searching/{customerID}",method = RequestMethod.GET, produces = "application/json") 
-	@ApiOperation(value = "get customer id")
-	public Customer findCustomerbyID(@PathVariable("customerId") Integer customerId)
-	{
-		System.out.println("Controller Searching for Customer id is ===============:"+customerId);
-		
-		
-		return customerRepository.findOne(customerId);    
-		
-		
- 
-	}*/
-	//find out customer id method invoke
-	
-	@ApiOperation(value = "get a customer")
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET,produces = "application/json")
+
+    @ApiOperation(value = "get a customer")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public Customer findCustomerbyID(@PathVariable int id) {
-	
-		    Customer customer =null;
-		    customer = customerRepository.findCustomerBycustomerId(id);
-		try{
-	        if(customer.getCustomerId()==id){
-	    	    customer= customerRepository.findOne(id); 
-	    	
-	        }else{
-	        	 Object obj = "Customer id Notfound";
-	        	 customer = (Customer) obj;
-	        	   System.out.println("Customer Id is not found");
-	    	
-                 }
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	    return customer;
-	}
-	
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@RequestMapping("searching/all customer") 
-	@ApiOperation(value = "get all customers")
-	public List<Customer> listCustomers()
-	{
-		System.out.println("Searching all customer details===============:\n");		
-		List<Customer> list=(List<Customer>) customerRepository.findAll();
-		//testing.add(nameVal);
-	return list;
-	}
-	
-	@RequestMapping(value="searching/{firstName}",method = RequestMethod.GET, produces = "application/json") 
-	@ApiOperation(value = "get customer names1")
-	public Customer findCustomerbyFirstName(@PathVariable("firstName") String firstName)
-	{
-	   
-		 Customer customer ;
-		 System.out.println("Controller Searching for Employee Firstname ===============:"+firstName );
-		 customer = customerRepository.findCustomerByfirstName(firstName);		  
-	       try{
-		    if(customer.getFirstName().equalsIgnoreCase(firstName)){
-		    	 
-		    	System.out.println("Customer First Name isfound:");
-		    	
-		    }else{
-		    	  Object obj ="Customer Name not found";
-		    	  customer = (Customer) obj;
-	           System.out.println("Customer FirstName is not found:");  
-	    }
-	       }catch(Exception e){
-	    	   e.printStackTrace();
-	       }
-		return customer;	
-	}
-	
-	/*@RequestMapping(value="searching/{lastName}",method = RequestMethod.GET, produces = "application/json") 
-	@ApiOperation(value = "get customer names2")
-	public Customer findCustomerbyLastName(@PathVariable("lastName") String lastName)
-	{
-		System.out.println("Controller Searching for Employee Lastname ===============:"+lastName );
-		 Customer customer = customerRepository.findCustomerBylastName(lastName);
-		
-		  
-	       List<Customer> list=null;
-		    if(customer.getFirstName()!=lastName){
-		    	System.out.println("Customer Last Name is not found");
-		    	
-		    }else{
-	           list = (List<Customer>) customer;    
-	    }
-		    return (Customer) list;
-	}*/
+
+        Customer customer = null;
+        customer = customerRepository.findCustomerBycustomerId(id);
+        try {
+            if (customer.getCustomerId() == id) {
+                customer = customerRepository.findOne(id);
+
+            } else {
+                Object obj = "Customer id Notfound";
+                customer = (Customer) obj;
+                System.out.println("Customer Id is not found");
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return customer;
+    }
+
+    @RequestMapping(value = "/allcustomers", method = RequestMethod.GET)
+    @ApiOperation(value = "get all customers")
+    public List<Customer> listCustomers() {
+        System.out.println("Searching all customer details===============:\n");
+        List<Customer> list = (List<Customer>) customerRepository.findAll();
+        //testing.add(nameVal);
+        return list;
+    }
+
+    @RequestMapping(value = "/byname/{firstName}", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "get customer names1")
+    public Customer findCustomerbyFirstName(@PathVariable("firstName") String firstName) {
+
+        Customer customer;
+        System.out.println("Controller Searching for Employee Firstname ===============:" + firstName);
+        customer = customerRepository.findCustomerByfirstName(firstName);
+        try {
+            if (customer.getFirstName().equalsIgnoreCase(firstName)) {
+
+                System.out.println("Customer First Name isfound:");
+
+            } else {
+                Object obj = "Customer Name not found";
+                customer = (Customer) obj;
+                System.out.println("Customer FirstName is not found:");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return customer;
+    }
+
 }
